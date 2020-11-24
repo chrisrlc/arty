808d548587fb8736ce5c36c5eee299203307c6eb
Christina Cortland <crcortland@gmail.com>
2020-11-23 21:14:58 -0500

    Pesky Semicolons: a Removal Story

--- api/routes/authentication.js
+++ api/routes/authentication.js
@@ -1,21 +1,21 @@
-const AuthenticationController = require("../controllers/authentication.controller");
-const express = require('express');
-const router = express.Router();
+const AuthenticationController = require("../controllers/authentication.controller")
+const express = require('express')
+const router = express.Router()
 
 router.post('/api/auth/register', async (req, res) => {
-  await AuthenticationController.create(req, res);
+  await AuthenticationController.create(req, res)
 })
 
 router.post('/api/auth/login', async (req, res) => {
-  await AuthenticationController.login(req, res);
+  await AuthenticationController.login(req, res)
 })
 
 router.post('/api/auth/logout', (req, res) => {
-  AuthenticationController.logout(req, res);
+  AuthenticationController.logout(req, res)
 })
 
 router.get('/api/auth/user', (req, res) => {
-  AuthenticationController.getUser(req, res);
+  AuthenticationController.getUser(req, res)
 })
 
-module.exports = router;
+module.exports = router
