95743f19896e281d292e3ede9377f334f0576998
Christina Cortland <crcortland@gmail.com>
2020-11-23 21:14:58 -0500

    Pesky Semicolons: a Removal Story

--- api/controllers/authentication.controller.js
+++ api/controllers/authentication.controller.js
@@ -1,32 +1,32 @@
-const db = require("../models");
-const User = db.users;
-const bcrypt = require('bcrypt');
+const db = require("../models")
+const User = db.users
+const bcrypt = require('bcrypt')
 
 // Create and save a new User
 async function create (req, res) {
-  const email = req.body.email;
-  const plainPassword = req.body.password;
-  const agreeToTerms = req.body.agreeToTerms;
+  const email = req.body.email
+  const plainPassword = req.body.password
+  const agreeToTerms = req.body.agreeToTerms
 
   // Validate request
   if (!agreeToTerms) {
     res.status(401).send({
       message: "Please agree to the terms and conditions before creating an account."
-    });
-    return;
+    })
+    return
   }
   if (!email || !plainPassword) {
     res.status(401).send({
       message: "Email or password can't be empty!"
-    });
-    return;
+    })
+    return
   }
   const existingUser = await User.findOne({where: {email: email}})
   if (existingUser) {
     res.status(401).send({
       message: "Email has already been taken. Please log in or choose another."
-    });
-    return;
+    })
+    return
   }
 
   // Create a User
@@ -37,13 +37,13 @@ async function create (req, res) {
 
   // Save User in the database and log in
   try {
-    req.session.user = await User.create(newUser);
-    res.send(req.session.user);
+    req.session.user = await User.create(newUser)
+    res.send(req.session.user)
   } catch (err) {
     res.status(500).send({
       message:
         err.message || "Some error occurred while creating the user."
-    });
+    })
   }
 }
 
@@ -56,40 +56,40 @@ async function login (req, res) {
   if (!req.body.email || !req.body.password) {
     res.status(401).send({
       message: "Email or password can't be empty!"
-    });
-    return;
+    })
+    return
   }
 
-  const email = req.body.email;
-  const plainPassword = req.body.password;
+  const email = req.body.email
+  const plainPassword = req.body.password
 
   // Find User in the database and set session
   try {
-    const user = await User.findOne({where: {email: email}});
+    const user = await User.findOne({where: {email: email}})
     if (user && await comparePasswords(plainPassword, user.password)) {
-      req.session.user = user;
-      res.send(req.session.user);
+      req.session.user = user
+      res.send(req.session.user)
     } else {
       res.status(403).send({
         message:
           "Invalid email or password."
-      });
+      })
     }
   } catch (err) {
     res.status(500).send({
       message:
         err.message || "Some error occurred while logging in."
-    });
+    })
   }
 }
 
 function logout (req, res) {
-  req.session.destroy();
-  res.redirect('/');
+  req.session.destroy()
+  res.redirect('/')
 }
 
 function getUser (req, res) {
-  res.send(req.session.user);
+  res.send(req.session.user)
 }
 
 module.exports = {
--- api/index.js
+++ api/index.js
@@ -1,29 +1,29 @@
-const authConfig = require("./config/auth.config.js");
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const authConfig = require("./config/auth.config.js")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const session = require('express-session')
 
-const app = express();
+const app = express()
 
 var corsOptions = {
   origin: 'http://localhost:3000',
   credentials: true
-};
+}
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-const db = require('./models');
-// db.sequelize.sync();
+const db = require('./models')
+// db.sequelize.sync()
 db.sequelize.sync({ force: true }).then(() => {
-  console.log('Drop and re-sync db.');
-});
+  console.log('Drop and re-sync db.')
+})
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 app.use(session({
@@ -37,14 +37,14 @@ app.use(session({
 
 // simple route
 app.get('/', (req, res) => {
-  res.json('Hola!!');
-});
+  res.json('Hola!!')
+})
 
 // bring in authentication routes
-app.use(require("./routes/authentication"));
+app.use(require("./routes/authentication"))
 
 // set port, listen for requests
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+  console.log(`Server is running on port ${PORT}.`)
+})
