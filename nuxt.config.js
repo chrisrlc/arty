cac4d0c6bc5aa9ddab22b35087788cb81fefa0d2
Christina Cortland <crcortland@gmail.com>
2021-02-04 09:52:01 -0500

    Disable axios debug

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,7 +82,7 @@ export default {
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
     baseURL: 'http://localhost:3000/api',
-    debug: true,
+    // debug: true,
     credentials: true
   },
 
