da39aafa38b902dcfd7de9dccc71e2b44e337b51
Christina Cortland <crcortland@gmail.com>
2021-02-17 10:38:24 -0500

    Fix sequelize deprecation warning

--- api/models/index.js
+++ api/models/index.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
-  operatorsAliases: false,
+  operatorsAliases: 0,
 
   pool: {
     max: 5,
